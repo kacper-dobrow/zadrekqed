ae9dd06ec416100d28a9e11fac07950a04aa313d
Kacper Dobrowolski <kacper.dobrow@gmail.com>
2024-06-02 19:59:01 +0200

    added flask app

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ import joblib
 import pandas as pd
 
 app = Flask(__name__)
-# models = joblib.load('models.pkl')
+models = joblib.load('models.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     input_data = request.json
     input_df = pd.DataFrame(input_data)
-    # predictions = np.mean([model.predict(input_df) for model in models], axis=0)
-    predictions = {"Hello": "World"}
-    # return jsonify(predictions.tolist())
-    return predictions
+    predictions = np.mean([model.predict(input_df) for model in models], axis=0)
+    return jsonify(predictions.tolist())
 
 
 if __name__ == '__main__':
